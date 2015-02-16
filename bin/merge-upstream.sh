07f3457783b75b41a545b4d52042528e9fdaa74f
dcsan <dc@pikkle.com>
2015-02-16 01:46:14 -0800

    upstream adds

---
+++ bin/merge-upstream.sh
@@ -0,0 +1,5 @@
+# just once:
+# git remote add upstream git@github.com:meteoric/meteor-ionic.git
+
+fetch upstream
+merge upstream/master
--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "dcsan:ionic",
   summary: "Ionic components for Meteor. No Angular!",
-  version: "0.1.14",
+  version: "0.1.15",
   git: "https://github.com/dcsan/meteor-ionic.git"
   // git: "https://github.com/meteoric/meteor-ionic.git"
 });
