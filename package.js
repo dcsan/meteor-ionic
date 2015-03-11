f1fce76d04f8d7d04a2790537ff08d64c893fc2b
dcsan <dc@pikkle.com>
2015-03-11 15:56:47 -0700

    bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "dcsan:ionic",
   summary: "Ionic components for Meteor. No Angular!",
-  version: "0.1.17_4",
+  version: "0.1.17_5",
   git: "https://github.com/dcsan/meteor-ionic.git"
   // git: "https://github.com/meteoric/meteor-ionic.git"
 });
